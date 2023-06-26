7433370f98a11932081c4fe1dc664e3461fefb6b
tomerschwartz2411 <tomerschwartz2411@gmail.com>
2023-06-26 08:08:07 +0300

    Mocking developer making changes

--- counter-service.py
+++ counter-service.py
@@ -6,10 +6,10 @@ counter = 0
 @app.route('/', methods=["POST", "GET"])
 def index():
     global counter
-    if request.method == "POST":
+    if request.method == "GET":
         counter+=1
         return "Hmm, Plus 1 please "
     else:
-        return str(f"Our counter is: {counter} ")
+        return str(f"This pointless app have : {counter} visitors, hooray! ")
 if __name__ == '__main__':
     app.run(debug=True,port=80,host='0.0.0.0')
